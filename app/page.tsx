bf0e5e40763c2245d97c1b3d99bcc9d70fc41fa0
Sai Sashreek D <saisashreekd@gmail.com>
2024-11-09 11:06:44 +0530

    Home Page Updated with new Content

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-5xl font-medium text-center text-emerald-500">Welcome To Education Video Platform.</h1>
+      <p className="text-3xl font-medium text-rose-500">Clerk is Configured</p>
     </>
   );
 }
